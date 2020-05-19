a5f706653655bec827b41aa66318e81daa878963
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2020-05-19 10:07:01 +0530

    Create web2.css

---
+++ web2.css
@@ -0,0 +1,132 @@
+body{
+	border: 2px solid rgba(10,200,199,0.8);
+	background:rgb(255,255,255);
+
+
+}
+td,
+th {
+    border: 5px solid rgb(190, 255, 190);
+    padding: 20px;
+   }
+
+td {
+    text-align: center;
+    color: rgb(2,90,118);
+}
+
+tr:nth-child(even) {
+    background-color: rgba(100,200,80,.2);
+}
+
+th[scope="col"] {
+    background-color:rgb(100,200,80);
+    color: #fff;
+}
+
+th[scope="row"] {
+    background-color: #d7d9f2;
+}
+
+caption {
+    padding: 10px;
+    caption-side: bottom;
+}
+
+table {
+     
+    border-collapse: collapse;
+    border: 2px solid rgb(200, 200, 20);
+    letter-spacing: 1px;
+    font-family: sans-serif;
+    font-size: 1.5rem;
+
+}
+h1
+{
+	border-right:540px;
+	border-left: 130px;
+	width: 700px;
+	color: rgb(230,59,99);
+	border: 2px solid yellow;
+	height: 60px;
+	background:rgba(100,255,110,.9);
+	text-align: center;
+	padding: 15px;
+	margin: auto;
+	
+	
+}
+
+h2{
+	color: blue;
+	margin:auto;
+}
+
+.ram
+{
+text-decoration-color: :rgb(200,70,90);
+border: 2px solid green;
+background: pink;
+width: 450px;
+height: 30px;
+padding-left: 165px;
+padding-top:9px;
+}
+#special1
+{
+    margin-bottom:799px;
+    border-collapse: collapse;
+    border: 2px solid rgb(200, 200, 20);
+    letter-spacing: 1px;
+    font-family: sans-serif;
+    font-size: 1.5rem;
+
+}
+#rani
+{
+	margin-top: 10px;
+}
+#viju{
+
+	margin-right: 10px;
+	color: rgb(255,20,110);
+	background:rgba(180,255,100,0.2);
+	border:4px solid rgba(100,200,255,0.3); 
+}
+h3
+{
+	text-align:center;
+
+	border:3px solid rgb(199,100,99);
+	width: 300px;
+	height: 30px;
+	background: rgba(200,99,100,.3);
+  margin:auto;
+  padding: 10px;
+}
+#ts
+{	
+ background:rgba(20,180,200,.2); 
+}
+image{
+	width: 80%;
+	margin: auto;
+
+}
+.Babloo
+{
+	color: green;
+	margin: auto;
+	text-align: center;
+	font-size:30px;
+	}
+#vkkk
+{
+	color:rgb(255,20,70);
+	text-align:center;
+	font-size:1.5rem;
+	text-decoration-color: red;
+	
+
+}
