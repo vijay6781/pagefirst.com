9430641536c0bd55bbe90d679e66cd7e63c7452e
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2020-06-27 18:08:51 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,32 +1,15 @@
 <?php
-$field_name = $_POST['cf_name'];
-$field_email = $_POST['cf_email'];
-$field_message = $_POST['cf_message'];
+if($_POST["submit"]) {
+$recipient = "mantukumarbunkar@gmail.com"; //my email
+$subject = 'Email message from Point Plumbing';
+$name = $_POST ["yourName"];
+$email = $_POST["yourEmail"];
+$phone = $_POST["yourPhone"];
+$location = $_POST["yourLocate"];
+$message = $_POST["message-me"];
 
-$mail_to = 'youremail@emaildomain.com';
-$subject = 'Your Subject'.$field_name;
-
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
-
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Sucess Message');
-		window.location = 'contact.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Error Message');
-		window.location = 'contact.html';
-	</script>
-<?php
+$mailBody="Name: $name\nEmail: $email\n\n$message";
+mail($recipient, $subject, $mailBody, "From: $name <$email>");
+$thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
 }
 ?>
